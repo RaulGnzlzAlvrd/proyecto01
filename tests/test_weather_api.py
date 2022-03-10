ea820433808fe64649eda59c7c4bf1ad73fc6357
Raúl González <gonzalva@ciencias.unam.mx>
2022-03-10 17:05:20 -0600

    Arregla los tests para mejor legibilidad

--- tests/test_weather_api.py
+++ tests/test_weather_api.py
@@ -1,27 +1,19 @@
 from unittest import TestCase
 
-from src.open_weather_api import get_weather
+from src.weather_api import get_clima
 
 class TestWeatherApi(TestCase):
-    def test_valid_code(self):
-        # Codigo iata válido
-        data = get_weather("MXN")
-        condition = "weather" in data.keys()
-        condition = condition and "main" in data.keys()
-        self.assertEqual(True, condition,
-                         "No se encontó la llave weather o main")
+    def test_valid_coords(self):
+        # Coordenadas válidas
+        lat, lon = '25.7785', '-100.107'
+        data = get_clima(lat, lon)
+        llaves = ["temperatura", "minima", "maxima", "descripcion",
+                  "sensacion_termica", "nubes"]
+        for key in llaves:
+            self.assertIn(key, data.keys(), f"No se encontró la llave {key}")
 
-    def test_valid_code_2(self):
-        data = get_weather("TLC")
-        condition = "weather" in data.keys()
-        condition = condition and "main" in data.keys()
-        self.assertEqual(True, condition,
-                         "No se encontó la llave weather o main")
-
-    def test_invalid_code(self):
-        # Codigo iata inválido
-        data = get_weather("holamundo")
-        condition = "weather" not in data.keys()
-        condition = condition and "main" not in data.keys()
-        self.assertEqual(True, condition,
-                         "Campos weather o main presentes, no deberían estar")
+    def test_invalid_coords(self):
+        # Coordenadas no válidas
+        lat, lon = '2222222222', '1111111111'
+        with self.assertRaises(Exception):
+            get_clima(lat, lon)
