2bf67589f18fbcbc267b087fdc6d40a7c54ec186
Raúl González <gonzalva@ciencias.unam.mx>
2022-03-10 22:07:02 -0600

    Agrega escritura a csv como salida del programa

--- src/get_lectura.py
+++ src/get_lectura.py
@@ -8,7 +8,18 @@ def process_row(row):
     d_lon = row["destination_longitude"]
     origen = get_clima(o_lat, o_lon)
     destino = get_clima(d_lat, d_lon)
-    print(origen, destino)
+    merge = list(origen.values()) + list(destino.values())
+    return merge
 
-df = pd.read_csv("./datasets/tickets.csv")
-df.apply(process_row, axis=1)
+def run():
+    print("Leyendo .csv")
+    df = pd.read_csv("./datasets/tickets.csv")
+    campos = ["descripcion", "temperatura", "minima", "maxima",
+              "sensacion_termica", "nubes"]
+    campos_origen = [f"origin_{campo}" for campo in campos]
+    campos_destino = [f"destination_{campo}" for campo in campos]
+    print("Obteniendo datos de clima")
+    df[campos_origen + campos_destino] = df.apply(process_row, axis=1, result_type="expand")
+    print("Escribiendo archivo de salida")
+    df.to_csv("./datasets/clima.csv")
+    print("Listo!")
